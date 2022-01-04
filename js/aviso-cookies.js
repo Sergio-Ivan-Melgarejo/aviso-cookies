fb801c5607e46cc2fe8ce843e497fba844d51851
Sergio <sergio.ivan.melgarejo@gmail.com>
2022-01-04 10:18:09 -0300

    Update aviso-cookies.js

--- js/aviso-cookies.js
+++ js/aviso-cookies.js
@@ -2,15 +2,14 @@ const botonAceptarCookies = document.getElementById("btn-aceptar-cookies");
 const avisoCookies = document.getElementById("aviso-cookies");
 const fondoCookies = document.getElementById("fondo-aviso-cookies");
 
-if(localStorage.getItem("cookies-aceptadas")) quitarCookies();
-
 const quitarCookies = () => {
     avisoCookies.classList.remove("activo");
     fondoCookies.classList.remove("activo");
 }
 
+if(localStorage.getItem("cookies-aceptadas")) quitarCookies();
+
 botonAceptarCookies.addEventListener("click",()=>{
     localStorage.setItem("cookies-aceptadas",true);
     quitarCookies();
 })
-
